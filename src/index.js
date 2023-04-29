802100c796ed7003595403d52f9c50e5595a28c2
hsin-rong <85164994+hsin-rong@users.noreply.github.com>
2023-04-29 16:22:02 +0800

    add firebase

--- src/index.js
+++ src/index.js
@@ -4,11 +4,31 @@ import './index.css';
 import App from './App';
 import './bootstrap.min.css'
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCfrLQpfC0bEIDICqPxl8YZfFjaNzGxAbU",
+    authDomain: "practice-ae625.firebaseapp.com",
+    projectId: "practice-ae625",
+    storageBucket: "practice-ae625.appspot.com",
+    messagingSenderId: "674064173449",
+    appId: "1:674064173449:web:9f37065fadf7e42fb1b958",
+    measurementId: "G-4FR2SHBJSF"
+  };
+
+
+const app = initializeApp(firebaseConfig);
+const database = firebase.database();
+const analytics = getAnalytics(app);  
+
+console.log(database);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+   
   </React.StrictMode>
 );
 
